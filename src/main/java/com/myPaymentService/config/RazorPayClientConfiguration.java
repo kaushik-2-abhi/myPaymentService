36d08d31a9e53dba5bb035ced7eb5ac9484ed2d1
kaushik-2-abhi <kaushik2abhi@gmail.com>
2024-07-13 19:12:49 +0530

    environment veriable set

--- src/main/java/com/myPaymentService/config/RazorPayClientConfiguration.java
+++ src/main/java/com/myPaymentService/config/RazorPayClientConfiguration.java
@@ -14,24 +14,12 @@ public class RazorPayClientConfiguration {
     @Value(("${RAZORPAY.KEY_SECRET}"))
    private String secretId;
 
-//    @Bean
-//    public RazorpayClient getRazorPayClient() throws RazorpayException {
-//        try{
-//        RazorpayClient razorpayClient=new RazorpayClient("rzp_test_w06Iua4sdQudcV","36RW6dXr5C9J0v1Dnml07J4A",true); return razorpayClient;
-//        }
-//        catch (RazorpayException e){
-//            e.printStackTrace();
-//            System.out.println(e);;
-//
-//        }
-//
-//        return null;
-//    }
+
 
     @Bean
     public RazorpayClient getRazorPayClient() throws RazorpayException {
         // Let exceptions propagate to Spring for better diagnostics
-        return new RazorpayClient("rzp_test_w06Iua4sdQudcV","36RW6dXr5C9J0v1Dnml07J4A");
+        return new RazorpayClient(keyId,secretId);
     }
 
 
